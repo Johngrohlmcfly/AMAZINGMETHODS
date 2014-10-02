a6beae0ba52456c2bef1e4ba568a3c2708777045
johngrohlmcfly <johnhndzr@gmail.com>
2014-10-02 03:52:35 -0500

    Cambios de stylo - Se Cambio el estilo de las funciones printf con descripciones detalladas de los datos requeridos

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 /**
- * Author: 
+ * Author: John & Sier <johnhndzr@gmail.com -- sierisimo@gmail.com>
  *
- * Date: 
- * LastUpdate: 
+ * Date:2/10/2014 
+ * LastUpdate:2/10/2014
  * version: 0.0.1~alpha1
  *
  * Name: methods.c
  *
- * Description:
- *   
+ * Description: This programm will make evaluate a function across 5 numeric methods.
+ *	
  *
  * Functions:
  *
@@ -19,8 +19,8 @@
 
 int main(int argc, const char *argv[])
 {
-	printf("Mensaje de bienvenida");
-	printf("Pedir datos");
-	
+	printf("\nBienvenido!!\n Este programa te ayudara a saber que metodo es la opcion mas efectiva para resolver tu ecuacion, dispondremos del metodo de Biseccion, del metodo de Falsa Posicion, del de Newton-Rapson y del metodo de la Secante.\n Empezaremos con los datos con el supuesto de que tu ecuacion es de la forma cuadratica F(x)= Ax^2 + Bx + C ...");
+	printf("\n\nIntroduce los datos para el intervalo [X,Y] respectivamente tecleando un enter entre cada dato:\n");
+	printf("\n\nAhora, deberas introducir el valor del ERROR:\n");
     return 0;
 }
--- src/set_variables.c
+++ src/set_variables.c
@@ -1,5 +1,6 @@
 #include"lib/methods.h"
 
 void set_coeficients(){
-	
+	printf("\n\nIntroduce los datos para el intervalo [X,Y] respectivamente tecleando un enter entre cada dato:\n");
+	printf("\n\nAhora, deberas introducir el valor del ERROR:\n");	
 }
