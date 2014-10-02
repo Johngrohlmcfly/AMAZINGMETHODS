bbb39650eddba9ad7e6d2a4a46eb2fb5870e052b
Sier Violencia <sierisimo@gmail.com>
2014-10-02 04:34:12 -0500

    More style on comments

--- src/set_variables.c
+++ src/set_variables.c
@@ -11,20 +11,28 @@
  * 		This file is in charge of setting the content of certain files
  *	
  * Functions:
- * 		set_coeficients:
- *			type: void
- *			returns: nothing
- *			description: Just get values for certainr variables
+ * 		set_coeficients
+ *
  */
 #include "../lib/methods.h"
 
+/**	set_init_variables
+ * 		type: void
+ *		returns: nothing
+ *		description: Just get values for certainr variables
+ * 
+ *  author:
+ *  LastUpdate:
+ */
 void set_init_variables(){
 	printf("\n\nIntroduce los datos para el intervalo [X,Y] respectivamente, separados por un espacio:\n");
+	//
 	if(scanf("%lf %lf",&inter_X,&inter_Y)!=2){
 		exit_with_error(1);
 	}
 	
 	printf("\n\nAhora, deberas introducir el valor del ERROR:\n");
+	//
 	if(scanf("%lf",&ERROR_VAL)!=1){
 		exit_with_error(1);
 	}
